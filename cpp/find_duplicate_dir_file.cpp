b4f48f5ad48641de4357e9545734ee22f5d1ee7f
selva <selva@debian-laptop>
2014-06-08 19:12:40 +0530

    Intial version of the directory search

---
+++ cpp/find_duplicate_dir_file.cpp
@@ -0,0 +1,12 @@
+// this file is meant for testing the algorithms by comparing two directory
+// for finding the duplicate files and measure time taken by each algorithm to 
+// search the directory 
+//
+// Steps to done
+// 1) Open a directory
+// 2) find all the files and directory 
+// 3) Create a database of all the files found 
+// 4) for every new file found compare it with the existing data base and report if the file already exist
+// 5) use different search algorithm to search 
+// 6) use arguement list option library to add command line option 
+// 7) OpenMPI / OpenCL can used for scaling 
