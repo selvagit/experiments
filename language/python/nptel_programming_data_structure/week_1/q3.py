4a88d030f060ddab6e75d02ef62b12b4fd8617e6
selvaraj <selvabe99@gmail.com>
2017-07-30 22:06:49 +0530

    Add week-1 files for quiz

---
+++ language/python/nptel_programming_data_structure/week_1/q3.py
@@ -0,0 +1,6 @@
+
+def f(m,n):
+     ans = 1
+     while (m - n >= 0):
+       (ans,m) = (ans*2,m-n)
+     return(ans)
---
+++ language/python/nptel_programming_data_structure/week_1/q4.py
@@ -0,0 +1,6 @@
+
+def g(x,y):
+     val = 0
+     while (x > y):
+       (val,x) = (val+1,x/y)
+     return(val)
