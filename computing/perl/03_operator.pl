2d4c55cb6aaf066df52a38beea83b844ddbb4eff
selvaraj <selvabe99@gmail.com>
2024-07-03 16:46:23 +0530

    Initial commit of perl test code

---
+++ computing/perl/03_operator.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+# Modules used
+use strict;
+use warnings;
+
+# Print function
+print("Operator\n");
+
+# define array 
+my @array = (1, 2, 3, 4);
+
+# use qw function
+my @arr2 = qw /This is a Perl Tutorial/;
+
+# accessing the array elements
+print "elements of the array: \n";
+print "$array[0]\n";
+print "$array[3]\n";
+
+# accessing the array elements
+print "elements of the array: \n";
+print "$arr2[0]\n";
+print "$arr2[1]\n";
+
