8231563b464fc67274c54499b6db9b1e58d23275
selvaraj <selvabe99@gmail.com>
2025-07-24 12:26:07 +0530

    Added a sample code for variable argument list

---
+++ computing/c/va_arg/va_arg.c
@@ -0,0 +1,22 @@
+
+#include <stdarg.h>
+#include <stdio.h>
+
+void print_nums(int first_arg, ...) {
+    va_list args;
+    va_start(args, first_arg);
+
+    printf("First argument: %d\n", first_arg);
+
+    int num;
+    while ((num = va_arg(args, int)) != 0) {
+        printf("Next argument: %d\n", num);
+    }
+
+    va_end(args);
+}
+
+int main() {
+    print_nums(1, 2, 3, 4, 0); // Example usage
+    return 0;
+}
